f8b4805c282c1cce14cadaaabee17df165eb0496
Ken Suon <ken.su_dev@hotmail.com>
2022-03-29 10:57:16 +0200

    Create a SingleLiveEvent for Toast Warning Message

--- app/src/main/java/com/suonk/oc_project5/ui/tasks/create/CreateTaskViewModel.java
+++ app/src/main/java/com/suonk/oc_project5/ui/tasks/create/CreateTaskViewModel.java
@@ -1,16 +1,25 @@
 package com.suonk.oc_project5.ui.tasks.create;
 
+import android.app.Application;
+import android.icu.text.UnicodeSetIterator;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.suonk.oc_project5.R;
 import com.suonk.oc_project5.model.data.Project;
 import com.suonk.oc_project5.model.data.Task;
 import com.suonk.oc_project5.repositories.project.ProjectRepository;
 import com.suonk.oc_project5.repositories.task.TaskRepository;
+import com.suonk.oc_project5.utils.SingleLiveEvent;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -24,33 +33,54 @@ public class CreateTaskViewModel extends ViewModel {
     @NonNull
     private final TaskRepository taskRepository;
 
-    private final ExecutorService executor = Executors.newFixedThreadPool(10);
+    @NonNull
+    private final ProjectRepository projectRepository;
+
+    @NonNull
+    private final Application context;
+
+    @NonNull
+    private final Executor executor;
+
+    @NonNull
+    private final SingleLiveEvent<String> toastSingleLiveEvent = new SingleLiveEvent<>();
+
 
     @Inject
-    public CreateTaskViewModel(@NonNull TaskRepository taskRepository) {
+    public CreateTaskViewModel(
+            @NonNull Application context,
+            @NonNull TaskRepository taskRepository,
+            @NonNull ProjectRepository projectRepository,
+            @NonNull Executor executor
+    ) {
+        this.context = context;
         this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
+        this.executor = executor;
     }
 
-    public void insertNewTask(CreateTaskViewState createTaskViewState) {
-        long projectId;
+    public LiveData<List<CreateTaskViewState>> getViewStatesLiveData() {
+        return Transformations.map(projectRepository.getAllProjects(), projects -> {
+                    List<CreateTaskViewState> createTaskViewStateList = new ArrayList<>();
 
-        switch (createTaskViewState.getProjectName()) {
-            case "Project Tartampion":
-                projectId = 1L;
-                break;
-            case "Project Lucidia":
-                projectId = 2L;
-                break;
-            case "Project Circus":
-                projectId = 3L;
-                break;
-            default:
-                projectId = 1L;
-                break;
+                    for (Project project : projects) {
+                        createTaskViewStateList.add(new CreateTaskViewState(
+                                project.getId(), project.getName()
+                        ));
+                    }
+
+                    return createTaskViewStateList;
+                }
+        );
+    }
+
+    public void insertNewTask(long projectId, @Nullable String taskName) {
+        if (taskName == null || taskName.isEmpty()) {
+            toastSingleLiveEvent.setValue(context.getString(R.string.create_task_dialog_empty_task_name_error), context);
+        } else {
+            executor.execute(() -> {
+                taskRepository.insertTask(new Task(0, projectId, taskName));
+            });
         }
-
-        executor.submit(() -> {
-            taskRepository.insertTask(new Task(0, projectId, createTaskViewState.getTaskName()));
-        });
     }
 }
